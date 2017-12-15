721a4000c0efa0d9cc49127813ce49ca5743e477
ssh <974125776@qq.com>
2017-12-15 19:34:33 +0800

    12151934

--- app/Http/Controllers/index/IndexController.php
+++ app/Http/Controllers/index/IndexController.php
@@ -23,102 +23,102 @@ class IndexController extends Controller
     // 用户首页
     public function index()
     {
-        return view('index/index');
+        return view('index/user/index');
     }
 
     // 用户详情
     public function information()
     {
-        return view('index/information');
+        return view('index/user/information');
     }
 
     // 用户修改密码
     public function password()
     {
-        return view('index/password');
+        return view('index/user/password');
     }
 
     // 用户修改电话
     public function bindphone()
     {
-        return view('index/bindphone');
+        return view('index/user/bindphone');
     }
 
     // 用户修改Email
     public function email()
     {
-        return view('index/email');
+        return view('index/user/email');
     }
 
     // 用户安全设置页面
     public function safety()
     {
-        return view('index/safety');
+        return view('index/user/safety');
     }
 
     // 用户地址管理
     public function address()
     {
-        return view('index/address');
+        return view('index/user/address');
     }
 
     // 用户订单管理
     public function order()
     {
-        return view('index/order');
+        return view('index/user/order');
     }
 
     // 用户退款页面
     public function change()
     {
-        return view('index/change');
+        return view('index/user/change');
     }
 
     // 用户退款详情
     public function record()
     {
-        return view('index/record');
+        return view('index/user/record');
     }
 
     // 用户收藏页面
     public function collection()
     {
-        return view('index/collection');
+        return view('index/user/collection');
     }
 
     // 用户访问页面
     public function foot()
     {
-        return view('index/foot');
+        return view('index/user/foot');
     }
 
     // 用户评价页面
     public function comment()
     {
-        return view('index/comment');
+        return view('index/user/comment');
     }
 
     // 用户评价页面
     public function commentlist()
     {
-        return view('index/commentlist');
+        return view('index/user/commentlist');
     }
 
     // 用户消息页面
     public function news()
     {
-        return view('index/news');
+        return view('index/user/news');
     }
 
     // 用户物流信息
     public function logistics()
     {
-        return view('index/logistics');
+        return view('index/user/logistics');
     }
 
     // 用户物流信息
     public function status()
     {
-        return view('index/status');
+        return view('index/user/status');
     }
 }
